e0e41714264d0d18b8e778801b37199962463abc
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-06-11 11:25:30 -0400

    inital setup

---
+++ src/components/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Result() {
+    return (
+        <div>
+            This is the Result
+        </div>
+    )
+}
+
+export default Result
---
+++ src/components/Text.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Text() {
+    return (
+        <div className='text'>
+            <p>
+            Insider Text
+            </p>
+        </div>
+    )
+}
+
+export default Text
+
+
